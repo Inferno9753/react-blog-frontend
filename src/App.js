f7fb138bf4d287c7804ba52d34d7b652af3ab035
Inferno <nishantkatekar58@gmail.com>
2023-09-12 11:09:47 +0530

    write page started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Topbar from "./components/topbar/Topbar";
+import Single from "./pages/single/Single";
+
+
+function App() {
+  return (
+    <div>
+      <Topbar/>
+      <Single/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,53 @@
+import './sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqYIP6aOhx3R4SJpE4Og75jOn_huCyNzESFvO7Zi29TQ&s"
+                    alt=""
+                />
+                <p>
+                    Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+                    amet ex esse.Sunt eu ut nostrud id quis proident.
+                </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        Life
+                    </li>
+                    <li className="sidebarListItem">
+                        Music
+                    </li>
+                    <li className="sidebarListItem">
+                        Sport
+                    </li>
+                    <li className="sidebarListItem">
+                        Style
+                    </li>
+                    <li className="sidebarListItem">
+                        Tech
+                    </li>
+                    <li className="sidebarListItem">
+                        Cinema
+                    </li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
