ddf8eb54b70b1bc7785b0ea948e077c73b65654d
Inferno <nishantkatekar58@gmail.com>
2023-09-12 12:33:02 +0530

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Topbar from "./components/topbar/Topbar";
+import Login from "./pages/login/Login";
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Register from "./pages/register/Register"
 
 
 function App() {
   return (
     <div>
       <Topbar/>
-      <Single/>
+      <Register/>
     </div>
   );
 }
